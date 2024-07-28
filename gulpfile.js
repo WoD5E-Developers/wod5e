5b345722b67c79c13c4901d5ff2fc0e38d1b2daf
Veilza <veilzakinsemi@gmail.com>
2024-07-27 19:27:46 -0500

    Now use Gulp and Less modules to compile CSS

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp')
+var less = require('gulp-less')
+var path = require('path')
+var concat = require('gulp-concat')
+
+// Create a task to take all .less files and convert them into CSS for Foundry
+// This includes a pipeline to turn it all into a single file so that hotreload works better
+gulp.task('less', function () {
+  return gulp.src('./display/**/styling/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(concat('wod5e-styling.css'))  // Concatenate all CSS files into a single file
+    .pipe(gulp.dest('./display/'))  // Output for the wod-styling.css file
+})
+
+// Watch for changes to .less files
+gulp.task('watch', function () {
+  gulp.watch('./display/**/styling/**/*.less', gulp.series('less'))
+})
+
+// Default task
+gulp.task('default', gulp.series('less', 'watch'))
