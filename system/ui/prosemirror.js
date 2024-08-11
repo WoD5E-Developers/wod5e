0e19f7c2f4d3d0bdc1d101b05301926c94d2411d
Veilza <veilzakinsemi@gmail.com>
2024-08-10 19:26:52 -0500

    Adjustments to the ProseMirror extension script

--- system/ui/prosemirror.js
+++ system/ui/prosemirror.js
@@ -2,24 +2,27 @@
 
 // Add to the existing Prosemirror dropdowns
 export const ProseMirrorSettings = async () => {
+  const toggleMark = foundry.prosemirror.commands.toggleMark
+  const spanMark = foundry.prosemirror.dom.parser.schema.marks.span
+
+  // Add a "World of Darkness" tab to the menu dropdowns
   Hooks.on('getProseMirrorMenuDropDowns', (menu, dropdowns) => {
-    const toggleMark = foundry.prosemirror.commands.toggleMark
     if ('format' in dropdowns) {
       dropdowns.format.entries.push({
         action: 'wod5e',
         title: 'World of Darkness',
         children: [{
-          action: 'wod5e-symbol',
+          action: 'wodSymbols',
           class: 'wod5e-symbol',
           title: 'abcdefghij',
-          mark: menu.schema.marks.span,
+          mark: spanMark,
           attrs: {
             _preserve: {
               class: 'wod5e-symbol'
             }
           },
           priority: 1,
-          cmd: toggleMark(menu.schema.marks.span, {
+          cmd: toggleMark(spanMark, {
             _preserve: {
               class: 'wod5e-symbol'
             }
