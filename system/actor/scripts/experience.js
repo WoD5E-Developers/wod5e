f9cb62cf4b6f888312ebf3c4aacf1812124bc545
Veilza <veilzakinsemi@gmail.com>
2024-09-01 19:18:06 -0500

    Linting

--- system/actor/scripts/experience.js
+++ system/actor/scripts/experience.js
@@ -72,7 +72,7 @@ export const _onRemoveExperience = async function (event) {
 
   // Define the actor's gamesystem, defaulting to "mortal" if it's not in the systems list
   const system = actor.system.gamesystem in WOD5E.Systems.getList({}) ? actor.system.gamesystem : 'mortal'
-  
+
   // Define the existing list of experiences
   let actorExperiences = actor.system.experiences || []
   const experienceToDelete = actorExperiences.find(exp => exp.id === experienceId)
@@ -103,7 +103,7 @@ export const _onRemoveExperience = async function (event) {
       callback: async () => {
         // Filter out the experience to be removed
         actorExperiences = actorExperiences.filter(exp => exp.id !== experienceId)
-        
+
         // Update the actor with the new list of experiences
         await actor.update({ 'system.experiences': actorExperiences })
       }
@@ -135,10 +135,10 @@ export const _onEditExperience = async function (event) {
 
   // Define the actor's gamesystem, defaulting to "mortal" if it's not in the systems list
   const system = actor.system.gamesystem in WOD5E.Systems.getList({}) ? actor.system.gamesystem : 'mortal'
-  
+
   // Find the experience to edit
   const experienceToEdit = (actor.system.experiences || []).find(exp => exp.id === experienceId)
-  
+
   if (!experienceToEdit) {
     console.error(`Experience with ID ${experienceId} not found.`)
 
@@ -165,17 +165,17 @@ export const _onEditExperience = async function (event) {
             // Get the updated name and value from the dialogue
             const name = html.find('[id=xpName]').val()
             const value = html.find('[id=xpValue]').val()
-            
+
             // Update the experience
             experienceToEdit.name = name
             experienceToEdit.value = value
-            
+
             // Define the existing list of experiences
             let actorExperiences = actor.system.experiences || []
-            
+
             // Replace the old experience with the updated one
             actorExperiences = actorExperiences.map(exp => exp.id === experienceId ? experienceToEdit : exp)
-            
+
             // Update the actor with the modified list
             await actor.update({ 'system.experiences': actorExperiences })
           }
