efe0e69cfed274f0abdad41ca767479c137f6a47
Veilza <veilzakinsemi@gmail.com>
2024-09-08 07:00:07 -0500

    Fix collapses for features

--- system/actor/scripts/on-toggle-collapse.js
+++ system/actor/scripts/on-toggle-collapse.js
@@ -1,11 +1,9 @@
-export const _onToggleCollapse = async function (event) {
+export const _onToggleCollapse = async function (event, target) {
   event.preventDefault()
 
-  const target = $(event.currentTarget)
+  $(target).toggleClass('active')
 
-  target.toggleClass('active')
-
-  const content = target.closest('.collapsible-container').find('.collapsible-content')
+  const content = $(target).closest('.collapsible-container').find('.collapsible-content')
 
   if (content.css('maxHeight') === '0px') {
     content.css('maxHeight', content.prop('scrollHeight') + 'px')
