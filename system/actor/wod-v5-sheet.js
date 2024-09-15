09848ab2672b8e74a2f2cb812061d1155deb7d32
Veilza <veilzakinsemi@gmail.com>
2024-09-15 17:01:56 -0500

    Fix an issue where the actors sidebar wouldn't update the actor's name

--- system/actor/wod-v5-sheet.js
+++ system/actor/wod-v5-sheet.js
@@ -186,12 +186,19 @@ export class WoDActor extends HandlebarsApplicationMixin(foundry.applications.sh
   }
 
   static async onSubmitActorForm (event, form, formData) {
+    let rerenderSidebar = false
+
     // Process submit data
     const submitData = this._prepareSubmitData(event, form, formData)
 
     // Overrides
     const overrides = foundry.utils.flattenObject(this.actor.overrides)
     for (const k of Object.keys(overrides)) delete submitData[k]
+
+    // Re-render the actors sidebar if we're updating the actor name
+    if (this.actor.name !== submitData.name) {
+      rerenderSidebar = true
+    }
 
     // Update the actor data
     await this.actor.update(submitData, {
@@ -212,6 +219,11 @@ export class WoDActor extends HandlebarsApplicationMixin(foundry.applications.sh
 
     // Re-render with the updated parts array
     this.render(false, { parts })
+
+    // Rerender the actors sidebar if we need to
+    if (rerenderSidebar) {
+      game.actors.render()
+    }
   }
 
   _configureRenderOptions(options) {
