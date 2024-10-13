d03bc3c0df9a6e1f0cbdf710503fd91e92221886
Veilza <veilzakinsemi@gmail.com>
2024-10-13 06:22:50 -0500

    Add an option to resist or give into frenzy

--- system/actor/vtm/scripts/frenzy-roll.js
+++ system/actor/vtm/scripts/frenzy-roll.js
@@ -1,4 +1,4 @@
-/* global game */
+/* global game, foundry */
 
 import { WOD5eDice } from '../../../scripts/system-rolls.js'
 
@@ -9,25 +9,48 @@ export const _onFrenzyRoll = async function (event) {
   // Top-level variables
   const actor = this.actor
 
-  // Secondary variables
-  const willpowerDicePool = getWillpowerDicePool(actor)
-  const humanity = actor.system.humanity.value
-  const dicePool = Math.max(willpowerDicePool + Math.floor(humanity / 3), 1)
-
-  WOD5eDice.Roll({
-    basicDice: dicePool,
-    title: game.i18n.localize('WOD5E.VTM.ResistingFrenzy'),
-    actor,
-    data: actor.system,
-    disableAdvancedDice: true,
-    callback: async (err, result) => {
-      if (err) console.log(err)
-
-      if (!result.rollSuccessful) {
-        await actor.update({ 'system.frenzyActive': true })
+  // Check whether the player wants to skip the check and go straight into Frenzy
+  const doFrenzyRoll = await foundry.applications.api.DialogV2.wait({
+    window: {
+      title: game.i18n.localize('WOD5E.VTM.ResistingFrenzy')
+    },
+    content: game.i18n.localize('WOD5E.VTM.FrenzyChoiceResistOrGiveIn'),
+    modal: true,
+    buttons: [
+      {
+        label: game.i18n.localize('WOD5E.VTM.Resist'),
+        action: true,
+      },
+      {
+        label: game.i18n.localize('WOD5E.VTM.GiveIn'),
+        action: false,
       }
-    }
+    ]
   })
+
+  if (doFrenzyRoll) {
+    const willpowerDicePool = getWillpowerDicePool(actor)
+    const humanity = actor.system.humanity.value
+    const dicePool = Math.max(willpowerDicePool + Math.floor(humanity / 3), 1)
+
+    WOD5eDice.Roll({
+      basicDice: dicePool,
+      title: game.i18n.localize('WOD5E.VTM.ResistingFrenzy'),
+      actor,
+      data: actor.system,
+      disableAdvancedDice: true,
+      callback: async (err, result) => {
+        if (err) console.log(err)
+
+        if (!result.rollSuccessful) {
+          actor.update({ 'system.frenzyActive': true })
+        }
+      }
+    })
+  } else {
+    // Automatically enter frenzy
+    actor.update({ 'system.frenzyActive': true })
+  }
 }
 
 // Calculate the dice for a Willpower roll
