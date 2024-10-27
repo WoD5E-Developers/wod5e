b3a69c1fa9e9ee3ba50aa72a1bd0e34bcc39e844
Veilza <veilzakinsemi@gmail.com>
2024-10-27 00:02:17 -0500

    Syncing items from compendiums/world items now working

--- system/item/scripts/item-syncing.js
+++ system/item/scripts/item-syncing.js
@@ -1,4 +1,4 @@
-/* global game, ui */
+/* global game, ui, foundry */
 
 export const _onSyncFromDataItem = async function (event) {
   event.preventDefault()
@@ -12,7 +12,7 @@ export const _onSyncFromDataItem = async function (event) {
   for (const compendium of compendiumsList) {
     const docs = await compendium.getDocuments()
 
-    const foundItems = docs.filter((item) => (item.flags.vtm5e.dataItemId === dataItemId))
+    const foundItems = docs.filter((item) => (item?.flags?.vtm5e?.dataItemId === dataItemId))
 
     compendiumDataItems.push(...foundItems)
   }
@@ -60,7 +60,64 @@ export const _onSyncToDataItems = async function (event) {
   event.preventDefault()
 
   const item = this.item
-  const itemData = item.system
+  const dataItemId = item.getFlag('vtm5e', 'dataItemId')
 
-  console.log(itemData)
+  const actorsList = game.actors
+  const actorDataItems = []
+
+  for (const actor of actorsList) {
+    const foundItems = actor.items.filter((item) => (item?.flags?.vtm5e?.dataItemId === dataItemId))
+
+    actorDataItems.push(...foundItems)
+  }
+
+  const totalCount = actorDataItems.length
+
+  // If there's no items to update, just let the user know
+  if (totalCount === 0) {
+    return ui.notifications.info(`There were no items found associated with Data Item ID "${dataItemId}"`)
+  } else {
+    const updateItemsConfirmed = await foundry.applications.api.DialogV2.wait({
+      window: {
+        title: game.i18n.localize('WOD5E.ItemsList.ConfirmOverwrite')
+      },
+      content: game.i18n.format('WOD5E.ItemsList.ConfirmOverwriteString', {
+        string: totalCount
+      }),
+      modal: true,
+      buttons: [
+        {
+          label: game.i18n.localize('WOD5E.Confirm'),
+          action: true
+        },
+        {
+          label: game.i18n.localize('WOD5E.Cancel'),
+          action: false
+        }
+      ]
+    })
+
+    if (updateItemsConfirmed) {
+      // Iterate through each item that we filtered and update them
+      actorDataItems.forEach((itemToUpdate) => {
+        // Ignore "points" because those are customized per-actor
+        delete item.system?.points
+        // Ignore "uses" because those can also be customized per-actor
+        delete item.system?.uses
+
+        const updates = {
+          name: item.name,
+          img: item.img,
+          system: {
+            ...item.system
+          }
+        }
+
+        // Update the item with the data pulled
+        itemToUpdate.update(updates)
+
+        ui.notifications.info(`Item "${itemToUpdate.name}" has been updated on actor "${itemToUpdate.actor.name}"`)
+      })
+    }
+  }
 }
