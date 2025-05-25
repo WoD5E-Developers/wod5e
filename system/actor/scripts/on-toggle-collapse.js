2535c8dfb3d323992ac17e07d6fb00c64f3aa2fc
Veilza <113429647+Veilza@users.noreply.github.com>
2025-05-25 12:06:28 -0500

    Fix collapse toggle

--- system/actor/scripts/on-toggle-collapse.js
+++ system/actor/scripts/on-toggle-collapse.js
@@ -1,11 +1,11 @@
 export const _onToggleCollapse = async function (event, target) {
   event.preventDefault()
 
-  const content = $(target).closest('.collapsible-container').find('.collapsible-content')
+  const content = target.closest('.collapsible-container').querySelector('.collapsible-content')
 
   if (content.css('maxHeight') === '0px') {
-    content.css('maxHeight', content.prop('scrollHeight') + 'px')
+    content.style.setProperty('maxHeight', content.prop('scrollHeight') + 'px')
   } else {
-    content.css('maxHeight', '0px')
+    content.style.setProperty('maxHeight', '0px')
   }
 }
