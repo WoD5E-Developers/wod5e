7eabde5f254338c7b47056b40279329c69a6b8bd
Veilza <113429647+Veilza@users.noreply.github.com>
2025-05-24 21:37:40 -0500

    Fixed the settings sidebar

--- system/ui/settings-sidebar.js
+++ system/ui/settings-sidebar.js
@@ -4,22 +4,24 @@
 export const RenderSettings = async () => {
   Hooks.on('renderSettings', async (_app, html) => {
     // Additional system information resources
-    const systemRow = html[0].querySelector('#game-details li.system')
+    const systemRow = html.querySelector('#settings .info .system')
 
-    const systemLinks = `<li class='external-system-links'>
+    const systemLinks = `<div class='external-system-links'>
         <a href='https://github.com/WoD5E-Developers/wod5e/releases' target='_blank'>${game.i18n.localize('WOD5E.Changelog')}</a>
         |
         <a href='https://wod5e-developers.github.io/' target='_blank'>${game.i18n.localize('WOD5E.Wiki')}</a>
-      </li>`
+      </div>`
 
     systemRow.insertAdjacentHTML('afterend', systemLinks)
 
     // License Section
-    const settingsAccess = html[0].querySelector('#settings-access')
-    const licenseInformation = `<h2>${game.i18n.localize('WOD5E.LicensedUnderDarkPack')}</h2>
-      <div id='license-information'>
-        ${game.i18n.localize('WOD5E.LicensedUnderDarkPackFulltext')}
-      </div>`
-    settingsAccess.insertAdjacentHTML('afterend', licenseInformation)
+    const accessSection = html.querySelector('#settings .access')
+    const licenseInformation = `<section class="license flexcol">
+      <h4 class="divider">${game.i18n.localize('WOD5E.LicensedUnderDarkPack')}</h4>
+        <section class="info" id='license-information'>
+          ${game.i18n.localize('WOD5E.LicensedUnderDarkPackFulltext')}
+        </section>
+    </section>`
+    accessSection.insertAdjacentHTML('afterend', licenseInformation)
   })
 }
