c782e0c5c7a29b880dcd5b4708a53c2f74f10cf0
Veilza <113429647+Veilza@users.noreply.github.com>
2025-05-27 15:37:55 -0500

    Fix an instance where the _onToggleCollapse function could error out if no target was found

--- system/actor/scripts/on-toggle-collapse.js
+++ system/actor/scripts/on-toggle-collapse.js
@@ -2,11 +2,13 @@ export const _onToggleCollapse = async function (event, target) {
   event.preventDefault()
 
   const content = target.closest('.collapsible-container').querySelector('.collapsible-content')
-  const computedMaxHeight = parseInt(window.getComputedStyle(content).maxHeight, 10)
+    if (content) {
+    const computedMaxHeight = parseInt(window.getComputedStyle(content).maxHeight, 10)
 
-  if (computedMaxHeight === 0) {
-    content.style.maxHeight = content.scrollHeight + 'px'
-  } else {
-    content.style.maxHeight = '0px'
+    if (computedMaxHeight === 0) {
+      content.style.maxHeight = content.scrollHeight + 'px'
+    } else {
+      content.style.maxHeight = '0px'
+    }
   }
 }
