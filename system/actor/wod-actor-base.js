b0c58703d282911cb726745e3ee0e5cedb1bcd03
Veilza <113429647+Veilza@users.noreply.github.com>
2025-08-20 17:06:57 -0500

    Linter fixes

--- system/actor/wod-actor-base.js
+++ system/actor/wod-actor-base.js
@@ -594,7 +594,7 @@ export class WoDActor extends HandlebarsApplicationMixin(foundry.applications.sh
 
         // Force a reflow and then re-enable the transition property
         // We have to tell eslint to ignore the no-void rule because it's genuinely useful here
-         
+        // eslint-disable-next-line no-void
         void contentElement[0].offsetHeight
         contentElement.css('transition', '')
       }
