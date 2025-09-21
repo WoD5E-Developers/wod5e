0b5b069d2c678fbb5580a3c3c5ef2407e3c3e4d2
Veilza <113429647+Veilza@users.noreply.github.com>
2025-09-21 15:46:38 -0500

    Linter

--- system/main.js
+++ system/main.js
@@ -1,4 +1,4 @@
-/* global CONFIG, Hooks, foundry, ChatMessage, game, ui, CONST, jscolor */
+/* global CONFIG, Hooks, foundry, ChatMessage, game, ui, jscolor */
 
 // Actor sheets
 import { WoDActor } from './actor/actor.js'
