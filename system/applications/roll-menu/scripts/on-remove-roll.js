74e4ddec026e820aaf80e65aba2ad5c7d84f6f20
Veilza <113429647+Veilza@users.noreply.github.com>
2025-11-22 22:06:42 -0600

    Add a prompt before deleting a saved roll

--- system/applications/roll-menu/scripts/on-remove-roll.js
+++ system/applications/roll-menu/scripts/on-remove-roll.js
@@ -6,24 +6,53 @@ export const _onRemoveSavedRoll = async function (event, target) {
     game.users.current.getFlag('wod5e', 'rollMenuSavedRolls')
   )
 
-  // Remove the roll
-  delete savedRolls[rollToRemove]
+  // Define the content of the Dialog
+  const content = `<p>
+    ${game.i18n.format('WOD5E.ConfirmDeleteDescription', {
+      string: savedRolls[currentActiveRoll].name
+    })}
+  </p>`
 
-  // If the roll was the one currently being viewed, reset the active roll to the first
-  // set roll, or just nothing
-  if (rollToRemove === currentActiveRoll) {
-    const keys = Object.keys(savedRolls)
-    const newActiveRoll = keys.length ? keys[0] : ''
+  // Prompt a dialog for the user to confirm they want to delete the item
+  const confirmDelete = await foundry.applications.api.DialogV2.wait({
+    window: {
+      title: game.i18n.localize('WOD5E.ConfirmDelete')
+    },
+    classes: ['wod5e', 'dialog'],
+    content,
+    modal: true,
+    buttons: [
+      {
+        label: game.i18n.localize('WOD5E.Confirm'),
+        action: true
+      },
+      {
+        label: game.i18n.localize('WOD5E.Cancel'),
+        action: false
+      }
+    ]
+  })
 
-    await game.users.current.setFlag('wod5e', 'rollMenuActiveRoll', newActiveRoll)
-  }
+  if (confirmDelete) {
+    // Remove the roll
+    delete savedRolls[rollToRemove]
 
-  // Update the saved rolls flag
-  await game.users.current.update({ [`flags.wod5e.rollMenuSavedRolls.-=${rollToRemove}`]: null })
+    // If the roll was the one currently being viewed, reset the active roll to the first
+    // set roll, or just nothing
+    if (rollToRemove === currentActiveRoll) {
+      const keys = Object.keys(savedRolls)
+      const newActiveRoll = keys.length ? keys[0] : ''
 
-  // Re-render the application window once settings are updated
-  const RollMenuApplication = foundry.applications.instances.get('wod5e-roll-menu')
-  if (RollMenuApplication) {
-    RollMenuApplication.render()
+      await game.users.current.setFlag('wod5e', 'rollMenuActiveRoll', newActiveRoll)
+    }
+
+    // Update the saved rolls flag
+    await game.users.current.update({ [`flags.wod5e.rollMenuSavedRolls.-=${rollToRemove}`]: null })
+
+    // Re-render the application window once settings are updated
+    const RollMenuApplication = foundry.applications.instances.get('wod5e-roll-menu')
+    if (RollMenuApplication) {
+      RollMenuApplication.render()
+    }
   }
 }
