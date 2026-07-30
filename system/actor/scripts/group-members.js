51ddf57a9455fe133b4319d01148935f2b2103ab
Veilza <113429647+Veilza@users.noreply.github.com>
2026-07-30 17:47:24 -0500

    Fix some weirdness related to groups that can happen due to bad data

--- system/actor/scripts/group-members.js
+++ system/actor/scripts/group-members.js
@@ -14,14 +14,30 @@ export const _addActor = async function (group, uuid) {
     return
   }
 
-  // Check if the actor is already in a group and if the group still exists
+  // Check if the actor is already in a group and if the group still exists as well
+  // as whether the actor is in the group
+
+  // We do this weirdness because there's circumstances, either through data mani-
+  // pulation or just bad data in general, where the actor's "group" value or the
+  // group's "members" array can get out of sync. Software is great.
+
+  // Anyway, if any of these three are false, we can allow the actor to be put onto
+  // a new group.
+
   const actorHasGroup = actor.system.group
   const groupExists = game.actors.get(actorHasGroup)
+  const actorIsInGroup = groupExists.system.members.find((a) => a.id)
 
-  if (actorHasGroup && groupExists) {
+  if (actorHasGroup && groupExists && actorIsInGroup) {
     ui.notifications.warn(`Actor ${actor.name} is already in an existing group.`)
 
     return
+  }
+
+  // If we determined that the actor wasn't in the group, but the group existed
+  // we should clean up the bad data
+  if (groupExists && !actorIsInGroup) {
+    await _removeMemberFromGroup(actor, groupExists)
   }
 
   // If the actor exists, is unique, and does not already belong to an existing group, continue
@@ -57,7 +73,12 @@ export const _removeActor = async function (event, target) {
   const group = this.actor
   const actor = fromUuidSync(uuid)
 
+  await _removeMemberFromGroup(actor, group)
+}
+
+export const _removeMemberFromGroup = async function (actor, group) {
   // Filter out the UUID from the members list
+  const uuid = actor.uuid
   const membersList = group.system.members.filter((actor) => actor !== uuid)
 
   // Update the group sheet with the new members list
